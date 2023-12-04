14f539c94be02da1563f35127368ca749e271f2d
LeKeu <lekeuffer@gmail.com>
2023-12-03 23:13:52 -0300

    ajustes

--- Assets/Scripts/Expressoes/AnalisProjAlgo/Omega.cs
+++ Assets/Scripts/Expressoes/AnalisProjAlgo/Omega.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Omega : MonoBehaviour
@@ -11,6 +10,7 @@ public class Omega : MonoBehaviour
         bool ehMenor = false;
         int vezesMenor = 0;
         string auxF = f, auxG = g;
+        /*
         for (int i = 1; i < 1000; i++)
         {
             f = auxF;
@@ -19,12 +19,12 @@ public class Omega : MonoBehaviour
             f = f.Replace("N", i.ToString());
             g = g.Contains("*" + c.ToString()) ? g.Replace("N", i.ToString()) : g.Replace("N", i.ToString()) + "*" + c.ToString();
 
-            ExpressionEvaluator.Evaluate(f, out float resultF);
-            ExpressionEvaluator.Evaluate(g, out float resultG);
+            UnityEditor.ExpressionEvaluator.Evaluate(f, out float resultF);
+            UnityEditor.ExpressionEvaluator.Evaluate(g, out float resultG);
 
             if (resultG < resultF) { ehMenor = true; vezesMenor++; } else { ehMenor = false; }
         }
-
+        */
         if (ehMenor)
         {
             return true;
